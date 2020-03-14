9b16b601599f95c7fa7e40c79c183a8379cdefb2
Richard Flynn <richardflynn@Administrators-MacBook-Pro.local>
2020-03-14 14:52:28 +0000

    refractor

--- src/test/java/browserstack/HomepageTests.java
+++ src/test/java/browserstack/HomepageTests.java
@@ -9,8 +9,6 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.*;
-
-import framework.pageObjects.PageObject;
 import framework.pageObjects.RentalcarScannerHomePage;
 
 public class HomepageTests {
@@ -36,6 +34,11 @@ public class HomepageTests {
 	public void tearDownTest() {
 		driver.close();
 	}
+	
+	final String PAGE_TITLE = "Find the best Car Rental Deals | Save up to 40% | rentalcar-scanner.com";
+	final String PAGE_META_DESC = "rentalcar-scanner.com compares car rental from 900+ companies worldwide. "
+			+ "Compare luxury, economy, and family car rental in 30,000+ locations. Best price guaranteed.";
+
 
 	@Test
 	public void isBookingEngineSearchWidgetLoaded_Test() {
@@ -45,14 +48,14 @@ public class HomepageTests {
 	@Test
 	public void title_Test() {
 		assertEquals("incorrect title", homePage.getPageTitle(),
-				"Find the best Car Rental Deals | Save up to 40% | rentalcar-scanner.com");
+				PAGE_TITLE);
 	}
 
 	@Test
 	public void metaDescription_Test() {
+
 		assertEquals("incorrect meta description", homePage.getMetaDescription(),
-				"rentalcar-scanner.com compares car rental from 900+ companies worldwide. "
-						+ "Compare luxury, economy, and family car rental in 30,000+ locations. Best price guaranteed.");
+				PAGE_META_DESC);
 
 	}
 
